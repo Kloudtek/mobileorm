6346bf3ecb4d3e855c87c244cd4388ac7a584c4f
Yannick Menager <ymenager@kloudtek.com>
2015-09-17 21:55:02 +0100

    In progress

--- src/main/java/com/kloudtek/mobileorm/ORMAnnotationProcessor.java
+++ src/main/java/com/kloudtek/mobileorm/ORMAnnotationProcessor.java
@@ -1,5 +1,7 @@
 package com.kloudtek.mobileorm;
 
+import com.sun.tools.javac.code.Symbol;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
@@ -7,6 +9,10 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Set;
 
 /**
@@ -18,7 +24,26 @@ public class ORMAnnotationProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         final Set<? extends Element> tableObjs = roundEnv.getElementsAnnotatedWith(DbTable.class);
-        System.out.println();
-        return false;
+        for (Element tableObj : tableObjs) {
+            String cName = ((Symbol.ClassSymbol) tableObj).getQualifiedName().toString() + "ORMPersister";
+            try {
+                JavaFileObject jfo = processingEnv.getFiler().createSourceFile(cName);
+
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "creating source file: " + jfo.toUri());
+
+                Writer writer = jfo.openWriter();
+                writeClass(tableObj,writer);
+
+                writer.close();
+            } catch (IOException e) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Error generating "+cName+" : "+e.getMessage());
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void writeClass(Element tableObj, Writer writer) {
+
     }
 }
